e2d94e8b693c921481cb32a36d6a8594e59fdc2d
XiaoqianXiao <xiao.xiaoqian.320@gmail.com>
2025-09-10 10:52:35 -0700

    change into ckpt-all

--- launch_first_level.sh
+++ launch_first_level.sh
@@ -8,7 +8,7 @@ scripts_dir="/gscratch/scrubbed/fanglab/xiaoqian/NARSAD/work_flows/firstLevel"
 
 for phaseID in 2 3; do
   PHASE_DIR="$scripts_dir/phase$phaseID"
-  final_scripts_dir="$PHASE_DIR/roi"
+  final_scripts_dir="$PHASE_DIR"
   #final_scripts_dir="$PHASE_DIR/searchlight"
   for script in "$final_scripts_dir"/*.sh; do
     sbatch "$script"
